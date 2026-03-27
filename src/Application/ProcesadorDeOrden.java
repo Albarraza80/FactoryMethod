f401556cb887bfd3ef5bac38cbc649b739271a04
Albarraza80 <albertbarraza@gmail.com>
2026-03-27 12:34:38 -0500

    Se habilita ProcesadorDeOrden

--- src/Application/ProcesadorDeOrden.java
+++ src/Application/ProcesadorDeOrden.java
@@ -7,30 +7,30 @@ import java.util.Scanner;
 
 public class ProcesadorDeOrden{
 
-//    public void tomarOrden(){
-//
-//        Scanner sc = new Scanner(System.in);
-//
-//        System.out.print( """
-//            ******MENÚ RESTAURANTE*****
-//            1. Arroz con coco
-//            2. Cojinoa al ajillo
-//            3. Higado encebollado
-//
-//            Seleccione un plato: >""" + " ");
-//
-//        int seleccion = sc.nextInt();
-//
-//        Orden ordenPedida = switch (seleccion) {
-//            case 1 -> PlatosOfrecidos.ARROZCOCO.crear();
-//            case 2 -> PlatosOfrecidos.COJINOAAJILLO.crear();
-//            case 3 -> PlatosOfrecidos.HIGADOENCEBOLLADO.crear();
-//            default -> {
-//                System.out.println("Opción no válida.");
-//                yield PlatosOfrecidos.ARROZCOCO.crear();
-//            }
-//        };
-//
-//      ordenPedida.fabricaDePlatos();
-//    }
+    public void tomarOrden(){
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print( """
+            ******MENÚ RESTAURANTE*****
+            1. Arroz con coco
+            2. Cojinoa al ajillo
+            3. Higado encebollado
+
+            Seleccione un plato: >""" + " ");
+
+        int seleccion = sc.nextInt();
+
+        Orden ordenPedida = switch (seleccion) {
+            case 1 -> PlatosOfrecidos.ARROZCOCO.crear();
+            case 2 -> PlatosOfrecidos.COJINOAAJILLO.crear();
+            case 3 -> PlatosOfrecidos.HIGADOENCEBOLLADO.crear();
+            default -> {
+                System.out.println("Opción no válida.");
+                yield PlatosOfrecidos.ARROZCOCO.crear();
+            }
+        };
+
+      ordenPedida.fabricaDePlatos();
+    }
 }
