5703b5776bffa9c0d5f9dfc69412e01a885ae34e
Albarraza80 <albertbarraza@gmail.com>
2026-03-27 19:37:21 -0500

    Se deshabilita el main

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import Application.ProcesadorDeOrden;
 
 public class Main{
     public static void main( String[] args ){
-        ProcesadorDeOrden tomadorDeOrden = new ProcesadorDeOrden();
-        tomadorDeOrden.tomarOrden();
+//        ProcesadorDeOrden tomadorDeOrden = new ProcesadorDeOrden();
+//        tomadorDeOrden.tomarOrden();
     }
 }
